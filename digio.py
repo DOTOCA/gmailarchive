a3c4bf40574bdc1310a4da0d060740fb20215406
Adam Wisniewski <adamw@tbcn.ca>
2013-02-16 18:12:25 -0500

    - externalizing DigitalOcean functionality - creating key, booting droplet works now

---
+++ digio.py
@@ -0,0 +1,77 @@
+from Crypto.PublicKey import RSA
+from Crypto.Util.randpool import RandomPool
+import dodo
+
+ssh_key_name = 'digio_default_ssh'
+droplet_name = 'digio_default_name'
+
+def status():
+    pass
+
+# Droplet
+
+def create_droplet(size=66, ssh_key=None, region=1, image=25306):
+    conn = dodo.connect()
+    dstart = conn.new_droplet(droplet_name, size, image, region, ssh_key)
+    return droplet(dstart['id'])
+
+def droplet_ids():
+    """
+    return a list of droplets named <droplet_name>
+    """
+    conn = dodo.connect()
+    return [d for d in conn.droplets() if d['name'] == droplet_name]
+
+def droplet(droplet_id=None):
+    # get droplet_id if not passed in
+    if droplet_id is None:
+        ds = droplet_ids()
+        if len(ds) > 0:
+            droplet_id =  ds[0]['id']
+        else:
+            return None
+
+    conn = dodo.connect()
+    droplet = conn.show_droplet(droplet_id)
+    return droplet
+
+# SSH
+
+def ssh_key_id():
+    """
+    Get the ID of the ssh_key up on DigitalOcean
+    """
+    conn = dodo.connect()
+    keys = [s for s in conn.ssh_keys() if s['name'] == ssh_key_name]
+
+    if len(keys) > 0:
+        ssh_key = conn.ssh_key(keys[0]['id'])
+
+        if ssh_key and ssh_key['ssh_pub_key'] == public_key():
+            return ssh_key['id']
+
+    return None
+
+def create_ssh_key():
+    """
+    Crate SSH key locally
+    """
+    key_size = 1024
+    pool = RandomPool(key_size)
+    pool.stir()
+    rsakey = RSA.generate(key_size, pool.get_bytes)
+
+    with open('private/gmailarchive_rsa', 'w') as k:
+        k.write(rsakey.exportKey('PEM'))
+    with open('private/gmailarchive_rsa.pub', 'w') as k:
+        k.write(rsakey.publickey().exportKey('OpenSSH'))
+
+def public_key():
+    """
+    Get the local public key string
+    """
+    key = ""
+    with open('private/gmailarchive_rsa.pub', 'r') as k:
+        key = k.read()
+    return key
+
--- fabfile.py
+++ fabfile.py
@@ -1,106 +1,74 @@
-from base64 import b64encode
-from Crypto.PublicKey import RSA
-from Crypto.Util.randpool import RandomPool
 from fabric.api import *
-from fabric.operations import *
+# from fabric.operations import *
 from fabric.colors import green as _green
 from fabric.colors import yellow as _yellow
 from fabric.colors import red as _red
-from fabric.contrib.files import *
+# from fabric.contrib.files import *
 import fabric
 
-import dodo
-import os
+from os.path import exists
 import sys
 import time
+import digio
 
 env.project_name = 'gmailarchive'
-droplet_name = 'gmailarchive'
-ssh_key_name = 'gmailarchive'
 
-def _always_run():
-    print 'ALWAYS RUNNING'
+digio.droplet_name = 'gmailarchive'
+digio.ssh_key_name = 'gmailarchive'
 
-def _dodo_servers():
-    """
-    return a list of servers named <droplet_name>
-    """
-    conn = dodo.connect()
-    droplets = [d for d in conn.droplets() if d['name'] == droplet_name]
-    return droplets
-
-def _dodo_ssh_key_id():
-    """
-    Get the ID of the ssh_key up on DigitalOcean
-    """
-    conn = dodo.connect()
-    keys = [s for s in conn.ssh_keys() if s['name'] == ssh_key_name]
-
-    if len(keys) > 0:
-        ssh_key = conn.ssh_key(keys[0]['id'])
-
-        if ssh_key and ssh_key['ssh_pub_key'] == _dodo_public_key():
-            return ssh_key['id']
-
-    return None
-
-def _dodo_create_ssh_key():
-    """
-    Crate SSH key locally
-    """
-    key_size = 1024
-    pool = RandomPool(key_size)
-    pool.stir()
-    rsakey = RSA.generate(key_size, pool.get_bytes)
-
-    with open('private/gmailarchive_rsa', 'w') as k:
-        k.write(rsakey.exportKey('PEM'))
-    with open('private/gmailarchive_rsa.pub', 'w') as k:
-        k.write(rsakey.publickey().exportKey('OpenSSH'))
-
-def _dodo_public_key():
-    """
-    Get the local public key string
-    """
-    key = ""
-    with open('private/gmailarchive_rsa.pub', 'r') as k:
-        key = k.read()
-    return key
-
-def _dodo_create_server():
-    conn = dodo.connect()
-    conn.new_droplet(droplet_name, size_id, 25306, 1, 4648)
-
-def dodo_start():
-    if not os.path.exists('private/gmailarchive_rsa.pub'):
-        _dodo_create_ssh_key()
+#---------------------------
+# DigitalOcean
+#---------------------------
+def docean_start():
+    if not exists('private/gmailarchive_rsa.pub'):
+        digio.create_ssh_key()
 
     # Make sure SSH KEY is there
-    ssh_key_id = _dodo_ssh_key_id()
+    ssh_key_id = digio.ssh_key_id()
     if not ssh_key_id:
-        print "\nPlease go to the DigitalOcean website and make sure you have",
-        print "an SSH key with the followign:\n"
-        print "name: \n" + ssh_key_name
-        print "\nvalue: \n" + _dodo_public_key()
+        print _yellow("\nPlease go to the DigitalOcean " + \
+                "website and make sure you have"),
+        print _yellow("an SSH key with the following:\n")
+        print _green("name: \n") + digio.ssh_key_name
+        print _green("\nvalue: \n") + digio.public_key()
         fabric.utils.abort("SSH keys")
 
-    # check for server
+    droplet = digio.droplet()
+
+    # Need to boot a droplet
+    if droplet is None:
         # check for backup
-            # restore backup
-        # create cerver
-        # start server
+            # restore
+        print _green("Creating droplet")
+        droplet = digio.create_droplet(ssh_key=ssh_key_id)
+        print _green("Created server with id: %s" % (droplet['id']))
 
-    conn = dodo.connect()
-    droplets = _dodo_servers()
+    # Wait for droplet to boot or restore
+    while droplet['status'] == 'new':
+        print _yellow("Droplet status is '%s' waiting for boot..." % \
+                (droplet['status']))
+        time.sleep(10)
+        droplet = digio.droplet(droplet['id'])
 
-    # dodo_create_server()
+def docean():
+    droplet = digio.droplet()
 
-    """
-    while len(droplets) < 0:
-        time.sleep(5)
-        droplets = _dodo_servers()
-    """
+    env.key_filename = 'private/gmailarchive_rsa'
+    env.hosts = [droplet['ip_address']]
+    env.user = 'root'
+    env.disable_known_hosts = True
 
+#---------------------------
+# Gmail Backup
+#---------------------------
+def gmailbackup_install():
+    sudo('apt-get -y install git')
 
+    if not exists('gmailbackup'):
+        print _green("Getting gmaailbackup")
+        # run('mkdir gmailbackup')
+        run('wget http://gmailbackup.googlecode.com/files/gmailbackup-20100324_0051.tgz')
+        run('tar -xzf gmailbackup-20100324_0051.tgz')
+    else:
+        print(_red('Already installed'))
 
-_always_run()
